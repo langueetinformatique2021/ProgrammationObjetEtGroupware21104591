6b853d8e52005fd14114a328f4d9be125cef6627
masterlangueetinformatique <masterlangueetinformatique@vbox>
2024-10-24 10:47:00 +0200

    TD4

--- Condition1.java
+++ Condition1.java
@@ -2,14 +2,15 @@ package atelier04;
 
 public class Condition1 {
 	public static void main(String[] args) {
-	int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+	int p1 = -1, p2 = 9, p3 = 8, p4 = 4, p5 = 8;
+	int max;
 	
 	if (p1 > p2) {
-		if (p1 > p3)
+		if (p1 > p3) 
 			max = p1;
-		else
-			max = p3;	
-	} 
+		else 
+			max = p3;
+	}
 	else {
 		if (p2 > p3)
 			max = p2;
@@ -17,8 +18,19 @@ public class Condition1 {
 			max = p3;
 	}
 	
-	System.out.println(p4);
-
+	System.out.println(max);
+	
+	
+	/** à tester avec les six cas possibles 
+	 *  p1 > p2 > p3
+	 *  p1 > p3 > p2
+	 *  p2 > p3 > p1
+	 *  p2 > p1 > p3	 
+	 *  p3 > p2 > p1
+	 *  p3 > p1 > p2	 
+	 */
+	
+	
+	
 	}
-
-} 
+}
