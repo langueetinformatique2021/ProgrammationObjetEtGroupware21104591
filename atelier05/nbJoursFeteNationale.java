89a64b0bd96da2be5f2457f27b28273bcca39674
masterlangueetinformatique <masterlangueetinformatique@vbox>
2024-11-07 11:26:36 +0100

    Exercice2 (partie 1)

--- atelier05/nbJoursFeteNationale.java
+++ atelier05/nbJoursFeteNationale.java
@@ -5,12 +5,12 @@ public class nbJoursFeteNationale {
 	/** point d'entrée d'un exécutable */
 	public static void main(String[] args) { 
 		// Instanciation de deux objets de type date
-		Date today = new Date(); 
-		Date dfin = new Date();
+		Date today = new Date(7, 11, 2024); 
+		Date FeteNat = new Date(14, 7, 2025);
 
-		today.Afficher(); dfin.Afficher();
+		today.Afficher(); FeteNat.Afficher();
 		do { today.Incrementer(); today.Afficher(); }
-		while (today.CompareTo(dfin));
+		while (today.CompareTo(FeteNat));
 		}
 
 }
